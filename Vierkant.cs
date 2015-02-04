b7c2b60fb32918d98f09c1d4f1e3a4f090c47db1
Roel <roel_vercauteren@hotmail.com>
2015-02-04 10:06:33 +0100

    klasse Vierkant toegevoegd

---
+++ Vierkant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitConsole
+{
+    class Vierkant
+    {
+        public int Zijde { get; private set; }
+        public Vierkant(int zijde)
+        {
+            Zijde = zijde;
+        }
+
+        public int Omtrek
+        {
+            get
+            {
+                return 4 * Zijde;
+            }
+        }
+    }
+}
